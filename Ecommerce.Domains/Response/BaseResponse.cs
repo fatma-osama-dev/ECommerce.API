2d75af73aa6ae288495abb6747477edbecdebce4
Fatma Osama <fatma.osama.dev@gmail.com>
2026-08-19 12:29:55 +0200

    Add project files.

---
+++ Ecommerce.Domains/Response/BaseResponse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Response
+{
+    public class BaseResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public T? Data { get; set; }
+
+
+        [JsonIgnore] // Ignore exceptions during serialization
+        public Exception? Exception { get; set; }
+
+        public BaseResponse(bool success, string message, T data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public BaseResponse(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public BaseResponse(bool success, string message, Exception exception)
+        {
+            Success = success;
+            Message = message;
+            Exception = exception;
+        }
+    }
+    }
