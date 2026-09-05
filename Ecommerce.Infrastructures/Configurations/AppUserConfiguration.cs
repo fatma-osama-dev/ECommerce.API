c2936fffc3c4282cd48fad8c010e10389b1ddbeb
Fatma Osama <fatma.osama.dev@gmail.com>
2026-09-05 19:31:04 +0200

    feat(auth): implement AppUser and Address identity domains with fluent configuration

---
+++ Ecommerce.Infrastructures/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Configurations
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.OwnsOne(u => u.Address, a =>
+            {
+                a.WithOwner(); 
+
+                
+                a.Property(p => p.Street).IsRequired().HasMaxLength(150);
+                a.Property(p => p.City).IsRequired().HasMaxLength(50);
+            });
+
+            builder.Property(a => a.DisplayName)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
